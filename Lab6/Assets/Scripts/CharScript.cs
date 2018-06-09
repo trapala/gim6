46fad4ec91d5e19ad85db1701217a16498d673be
trapala <user@mail.eu>
2018-06-09 16:13:25 +0200

    no message

---
+++ Lab6/Assets/Scripts/CharScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharScript : MonoBehaviour {
+
+	private Animator animator;
+	private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+		animator.SetBool("Nodding", m_Jump);
+	}
+}
---
+++ Lab6/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+	public GameObject door;
+	public bool open;
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = door.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<Collider>().CompareTag("Player"))
+		{
+			animator.SetBool("Open", open);
+		}
+	}
+}
